d665abc7f2a3af9a4de9f198e1c676a39183f096
antoine <antoine.guingand@wanadoo.fr>
2024-01-18 16:26:31 +0100

    Add settings singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Settings\Settings;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->singleton(Settings::class);
     }
 }
